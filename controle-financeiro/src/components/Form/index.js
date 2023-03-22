3b10c35797ce073c0cfedb9de44920412e0fb847
Carolina Ariadne <c.ariadneadv@gmail.com>
2023-03-22 15:15:45 -0300

    inputs-form

--- controle-financeiro/src/components/Form/index.js
+++ controle-financeiro/src/components/Form/index.js
@@ -2,11 +2,43 @@ import React, { useState } from 'react';
 import * as C from './styles';
 
 const Form = () => {
-  const [desc, serDesc] = useState(''); // descrição
+  const [desc, setDesc] = useState(''); // descrição
   const [amount, setAmount] = useState(''); // valor
-  const [isExpense, setExpense] = useState(false); // cheque
+  const [isExpense, setExpense] = useState(false); // check
 
-  return <div>Form</div>;
+  return (
+    <C.Container>
+      <C.InputContainer>
+        <C.Label>Descrição</C.Label>
+        <C.Input value={desc} onChange={e => setDesc(e.target.value)} />
+      </C.InputContainer>
+      <C.InputContainer>
+        <C.Label>Valor</C.Label>
+        <C.Input
+          value={amount}
+          type="number"
+          onChange={e => setAmount(e.target.value)}
+        />
+      </C.InputContainer>
+      <C.RadioGroup>
+        <C.Input
+          type="radio"
+          id="rIncome"
+          defaultChecked
+          name="group1"
+          onChange={() => setExpense(!isExpense)}
+        />
+        <C.Label htmlFor="rIncome">Entrada</C.Label>
+        <C.Input
+          type="radio"
+          id="rExpenses"
+          name="group1"
+          onChange={() => setExpense(!isExpense)}
+        />
+        <C.Label htmlFor="rExpenses">Saída</C.Label>
+      </C.RadioGroup>
+    </C.Container>
+  );
 };
 
 export default Form;
